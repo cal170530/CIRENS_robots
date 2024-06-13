8d61942536e1cce65ae7f0d62b348baadc53e600
cal170530 <68614731+cal170530@users.noreply.github.com>
2024-06-13 14:47:36 -0500

    Add files via upload

--- agentcontrol_pkg/agentcontrol_pkg/main.py
+++ agentcontrol_pkg/agentcontrol_pkg/main.py
@@ -2,33 +2,42 @@ import rclpy
 from rclpy.executors import SingleThreadedExecutor
 import sys
 from agentcontrol_pkg.agentcontrol import AgentController
-
+from agentcontrol_pkg.agentcontrol_sim import AgentController_Sim
 
 def main():
     rclpy.init()
     executor = SingleThreadedExecutor()
-    agents = 2
+    agents = [1,2]
     namespace = 'robot'
+    sim = False
 
-    if len(sys.argv) > 1:
-        namespace = str(sys.argv[1])
-        agents = int(sys.argv[2])
-    N= []
+    #if len(sys.argv) == 1:
+    match(len(sys.argv)):
+        case 2:
+            sim = sys.argv[1]
+        case 3:
+            namespace = str(sys.argv[2])
+        case 4:
+            agents = int(sys.argv[3])
+        
+    N= dict.fromkeys(agents)
     controllers = []
-    for i in range(agents):
+    for agent in agents:
         Ni =[]
-        for j in range(agents):
-            Ni.append('robot'+str(j))
-        N.append(Ni)
+        for neighbor in agents:
+            Ni.append(namespace+str(neighbor))
+        N[agent] = Ni
     
     
 
     
     
     try:
-        for i in range(agents): 
-              
-            controller = AgentController(i,neighbors = N[i])
+        for agent in agents: 
+            if sim:
+                controller = AgentController_Sim(agent,neighbors = N[agent])
+            else: 
+                controller = AgentController(agent,neighbors = N[agent])
             controllers.append(controller)
       
         for controller in controllers:
